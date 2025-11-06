722e54734999c733b34ea6096df8823962317348
gabriela <gabipinheiro920@hmail.com>
2025-11-05 21:39:38 -0300

    2

--- aularepeticao2.py
+++ aularepeticao2.py
@@ -1,12 +1,28 @@
+print("\nCadastro de usuario\n")
+usuario_cadastrado = input("Crie seu nome de usuario:").strip().lower()
+
+
+print("\nUsuario cadastrado com sucesso!\n")
+
+while True:
+    senha_cadastrada = input("Crie uma senha de usuario: (máx. 8 caracteres): ").strip()
+    
+    if len(senha_cadastrada) <= 8:
+        print("\nUsuario cadastrado com sucesso!\n")
+        break
+    else:
+        print("Senha muito longa! Digite uma senha com até 8 caracteres.\n")
+
+
+print("\n Login \n")
 #Inicia um loop que vai se repetir enquanto a condicao for verdadeira
 while True:#True é uma condicao booleana que sempre e verdadeira,logo esse loop será infinito
-    nome = input("Usuario: ")
-    senha = input("senha: ")
+    nome = input("Usuario: ").strip().lower()
+    senha = input("senha: ").strip()
 
     #condicao que faz uma comparacao para verificar se ambas condicoes sao verdadeiras 
-    if nome == "admin" and senha == "1234":
-        print("Login realizado com sucesso!")
+    if nome == usuario_cadastrado and senha == senha_cadastrada:
+        print(f"Login realizado com sucesso!\n Bem-vindo(a) {nome}")
         break #comando que interrompe imediatamente o loop
     else: 
         print("Usuario ou senha incorretos! Tente novamente...")
-        
