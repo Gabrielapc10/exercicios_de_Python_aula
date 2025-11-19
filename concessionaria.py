838b5b667a65fd24467f09da790dfce97a35c7c9
gabriela <gabipinheiro920@hmail.com>
2025-11-18 21:40:15 -0300

    1

---
+++ concessionaria.py
@@ -0,0 +1,24 @@
+cliente = {
+    "nome": input("Qual seu nome: "),
+    "telefone": input("Seu telefone: "),
+    "Saldo": float(input("Digite seu saldo inicial: "))
+}
+
+carros_modelo_marca = [{
+ "marca": "hyunday" ,
+ "modelo": "Genesis",
+ "preco":109000
+  },
+{"marca" : "BMw",
+ "modelo":"BMW X5" ,
+"preco":112000 
+},
+ {"marca": "Toyota",
+ "modelo": "Corola",
+"preco":101200
+ }
+ ]
+
+
+
+
